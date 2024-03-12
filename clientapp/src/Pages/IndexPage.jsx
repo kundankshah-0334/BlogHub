74e8c8b371466926cbf4d43463a398448117a102
kundan kumar sahu <kundanlal96580@gmail.com>
2024-03-12 23:10:40 +0530

    2773

--- clientapp/src/Pages/IndexPage.jsx
+++ clientapp/src/Pages/IndexPage.jsx
@@ -4,14 +4,31 @@ import Post from '../component/post/Post'
 function IndexPage() {
     const [posts , SetPosts] = useState([]);
 
-    useEffect(() => {
-        fetch('https://blog-hub-api.vercel.app/post').then(responce => {
-          responce.json().then(posts => {
-            SetPosts(posts);
-          })
-        })
+    // useEffect(() => {
+    //     fetch('https://blog-hub-api.vercel.app/post').then(responce => {
+    //       responce.json().then(posts => {
+    //         SetPosts(posts);
+    //       })
+    //     })
     
-      },[]);
+    //   },[]);
+
+    useEffect(() => {
+      fetch('https://blog-hub-api.vercel.app/post')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+          }
+          return response.json();
+        })
+        .then(posts => {
+          SetPosts(posts);
+        })
+        .catch(error => {
+          console.error('Fetch error:', error);
+          // Handle the error accordingly, e.g., show an error message to the user
+        });
+    }, []);
 
   return (
      <>
